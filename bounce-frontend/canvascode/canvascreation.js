dc4a3ea6d5372b547365522ba969baad428ab9a5
matteoaricci <matteoaricci@DESKTOP-KID7KUE.localdomain>
2020-03-20 12:51:52 -0400

    finally made a grid

--- bounce-frontend/canvascode/canvascreation.js
+++ bounce-frontend/canvascode/canvascreation.js
@@ -1,20 +1,20 @@
-let canvas = document.getElementById("myCanvas");
+const canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+let incrementValue = 50;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function createGrid() {
+    for (let i = 1; i < canvas.offsetWidth; i += incrementValue) { 
+        ctx.beginPath();
+        ctx.moveTo(i, 0)
+        ctx.lineTo(i, canvas.offsetHeight)
+        ctx.stroke()
+    }
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+    for (let i = 1; i < canvas.clientHeight; i+= incrementValue) {
+        ctx.beginPath();
+        ctx.moveTo(0, i)
+        ctx.lineTo(canvas.clientWidth, i)
+        ctx.stroke()
+    }
+}
+createGrid()
