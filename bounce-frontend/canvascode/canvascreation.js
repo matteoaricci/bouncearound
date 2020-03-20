d4be201d3134e08cc12e8eeee769194e16a76d6d
matteoaricci <matteoaricci@DESKTOP-KID7KUE.localdomain>
2020-03-20 15:04:27 -0400

    Animate Ball w collision

--- bounce-frontend/canvascode/canvascreation.js
+++ bounce-frontend/canvascode/canvascreation.js
@@ -1,20 +1,33 @@
 const canvas = document.getElementById("myCanvas");
+const horizDiv = document.getElementById('grid-horizontal')
+const vertDiv = document.getElementById('grid-vertical')
 let ctx = canvas.getContext("2d");
 let incrementValue = 50;
 
-function createGrid() {
+function createGrid(color = '#000000') {
+
     for (let i = 1; i < canvas.offsetWidth; i += incrementValue) { 
+        let newDiv = document.createElement('div')
         ctx.beginPath();
         ctx.moveTo(i, 0)
         ctx.lineTo(i, canvas.offsetHeight)
-        ctx.stroke()
-    }
+        newDiv.append(ctx.stroke())
+        newDiv.className = 'horizontal-line'
+        newDiv.id = `h-${(i / 50) + 0.98}`
+        horizDiv.append(newDiv)
+        ctx.strokeStyle = color
+    } 
 
     for (let i = 1; i < canvas.clientHeight; i+= incrementValue) {
+        let newDiv = document.createElement('div')
         ctx.beginPath();
         ctx.moveTo(0, i)
         ctx.lineTo(canvas.clientWidth, i)
-        ctx.stroke()
+        newDiv.append(ctx.stroke())
+        newDiv.className = 'vertical-line'
+        newDiv.id = `v-${(i / 50) + 0.98}`
+        vertDiv.append(newDiv)
+        ctx.strokeStyle = color
     }
 }
 createGrid()
