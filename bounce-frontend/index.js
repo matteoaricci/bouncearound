abed302d3395b8629f25c5b8f99a5d6937c416e3
matteoaricci <matteoaricci@DESKTOP-KID7KUE.localdomain>
2020-03-24 16:47:32 -0400

    doing some css

--- bounce-frontend/index.js
+++ bounce-frontend/index.js
@@ -1,3 +1,15 @@
+window.onload=function(){
+    let introAudio = document.createElement('audio')
+    let body = document.getElementById('body')
+    introAudio.src = "../bounce-frontend/assets/audio/gametheme.mp3"
+    introAudio.id = "intro-audio"
+    introAudio.loop = "loop"
+    introAudio.dataset.status = "on"
+    body.append(introAudio)
+    // introAudio.play()
+  }
+
+
 document.addEventListener('DOMContentLoaded', () => {
     homeScreen()
     let gameTitleHomeBtn = document.getElementById("game-title")
@@ -21,24 +33,49 @@ function homeScreen() {
 }
 
 function welcomePageSetup() {
+    let newSpan = document.createElement('span')
     let navBar = document.getElementById("navbar")
     let mainBox = document.getElementById("main")
     let enter = document.createElement("h1")
     let loginBtn = document.createElement("button")
     let guestBtn = document.createElement("button")
-
+    let audioBtn = document.createElement("button")
+    let newDiv = document.createElement('div')
+    
     navBar.innerText = ""
     mainBox.innerText = ""
     enter.id = "enter"
     enter.innerText = "ENTER"
     loginBtn.id = "login-btn"
+    loginBtn.className = 'btn btn-light'
     loginBtn.innerText = "Login"
     guestBtn.id = "guest-btn"
+    guestBtn.className = 'btn btn-light'
+    newDiv.className = 'login-buttons'
     guestBtn.innerText = "Continue As Guest"
+    audioBtn.innerText = "Audio"
+    audioBtn.id = "audio-btn"
+    audioBtn.className = 'btn btn-light'
+    audioBtn.addEventListener("click", pauseSound)
+    newDiv.append(loginBtn, guestBtn, audioBtn)
 
     loginBtn.addEventListener("click", renderLogin)
     guestBtn.addEventListener("click", renderGuest)
-
-    mainBox.append (enter, loginBtn, guestBtn)
+    newSpan.append(enter)
+    mainBox.append (newSpan, newDiv)
+    flickerText(`${enter.id}`)
 }
 
+function pauseSound() {
+    let introAudio = document.getElementById("intro-audio")
+    console.log(introAudio.dataset.status)
+    if (introAudio.dataset.status === "on"){
+        introAudio.pause()
+        introAudio.dataset.status =  "off"
+        console.log(introAudio.dataset.status)
+    }else {
+        introAudio.play()
+        introAudio.dataset.status = "on"
+        console.log(introAudio.dataset.status)
+    }
+}
---
+++ bounce-frontend/neonFlicker.js
@@ -0,0 +1,18 @@
+function flickerText(tagId) {
+
+const target = window.document.getElementById(tagId)
+
+const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+const colorLetter = letter => `<span style="color: hsla(360, 100%, 100%, 1);">${letter}</span>`;
+const flickerAndColorText = text => 
+  text
+    .split('')
+    .map(flickerLetter)
+    .map(colorLetter)
+    .join('');
+const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
+
+
+neonGlory(target);
+target.onclick = ({ target }) =>  neonGlory(target);
+}
